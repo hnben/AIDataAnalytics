e91de9953bffd79368c117381597f50a9f1d0da6
Huy Nguyen <hnguyen10082000@gmail.com>
2024-02-16 23:18:25 -0500

    added controller with routing

---
+++ controllers/controller.php
@@ -0,0 +1,67 @@
+<?php
+class Controller{
+    private $_f3;
+
+    public function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    function home ()
+    {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+
+    function login ()
+    {
+        // Display a view page
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $username = $_POST['username'];
+            $password= $_POST['password'];
+
+            //hardcoded login right now
+            if ($username == "user" AND $password == "pass"){
+                $this->_f3->reroute('overview');
+            }
+
+            else{
+                $this->_f3->set('SESSION.loginStatus', "login failed");
+            }
+        }
+        $view = new Template();
+        echo $view->render('views/login.html');
+    }
+
+    function overview ()
+    {
+        // Display a view page
+        $view = new Template();
+        echo $view->render('views/overview.html');
+    }
+
+    function expense ()
+    {
+        $view = new Template();
+        echo $view->render('views/expense.html');
+    }
+
+    function revenue()
+    {
+        $view = new Template();
+        echo $view->render('views/revenue.html');
+    }
+
+    function sales ()
+    {
+        $view = new Template();
+        echo $view->render('views/sales.html');
+    }
+
+    function trends()
+    {
+        $view = new Template();
+        echo $view->render('views/trends.html');
+    }
+
+}
--- index.php
+++ index.php
@@ -14,75 +14,53 @@ require_once('vendor/autoload.php');
 
 // Instantiate Fat-Free framework (F3)
 $f3 = Base::instance(); //static method
+$controller = new Controller($f3);
 
 // Define a default route
 $f3->route('GET /', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 // Routing a home route
 $f3->route('GET /home', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/home.html');
+    $GLOBALS['controller']->home();
 });
 
 // Routing to the Login Page
 $f3->route('GET|POST /login', function($f3) {
-
-    // Display a view page
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $username = $_POST['username'];
-        $password= $_POST['password'];
-
-        //hardcoded login right now
-        if ($username == "user" AND $password == "pass"){
-            $f3->reroute('overview');
-        }
-
-        else{
-            $f3->set('SESSION.loginStatus', "login failed");
-        }
-    }
-        $view = new Template();
-        echo $view->render('views/login.html');
+    $GLOBALS['controller']->login();
 });
 
 // Routing to the Overview Page
 $f3->route('GET|POST /overview', function() {
-    // Display a view page
-    $view = new Template();
-    echo $view->render('views/overview.html');
+    $GLOBALS['controller']->overview();
 });
 
 // Routing to the Expense Page
 $f3->route('GET|POST /expense', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/expense.html');
+    $GLOBALS['controller']->expense();
 });
 
 // Routing to the Revenue Page
 $f3->route('GET|POST /revenue', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/revenue.html');
+    $GLOBALS['controller']->revenue();
 });
 
 // Routing to the Sales Page
 $f3->route('GET|POST /sales', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/sales.html');
+    $GLOBALS['controller']->sales();
 });
 
 // Routing to the Trends Page
 $f3->route('GET|POST /trends', function() {
     // Display a view page
-    $view = new Template();
-    echo $view->render('views/trends.html');
+    // Display a view page
+    $GLOBALS['controller']->trends();
 });
 
 
