817b7b96ede945b1f585cc30739ed221df2ed1eb
Will Castillo <william.castillo.jr@outlook.com>
2024-03-13 20:15:06 -0400

    Modified class to include transactionID and category fields. Added fields into constructor

--- classes/order.php
+++ classes/order.php
@@ -1,13 +1,17 @@
 <?php
 
 class Orders extends Revenue{
+    private $_transactionID;
+    private $_category;
     private $_item; // String
     private $_amount; // String
 
-    public function __construct($type, $date, $totalAmount,
-                                $item, $amount)
+    public function __construct($date, $totalAmount,
+                                $transactionID, $category, $item, $amount)
     {
-        parent::__construct($type, $date, $totalAmount);
+        parent::__construct($date, $totalAmount);
+        $this->_transactionID = $transactionID;
+        $this->_category = $category;
         $this->_item = $item;
         $this->_amount = $amount;
     }
