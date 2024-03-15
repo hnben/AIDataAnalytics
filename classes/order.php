a6e5a36d418d2211695444f9618c547a74a1bb81
Will Castillo <william.castillo.jr@outlook.com>
2024-03-15 01:12:54 -0400

    Added Docblocks and removed getQuantity function

--- classes/order.php
+++ classes/order.php
@@ -1,11 +1,26 @@
 <?php
 
+/**
+ * The Orders class represents an order from a restaurant.
+ * This extends the Revenue class.
+ *
+ * @author Huy Nguyen, Tien Nguyen, Will Castillo
+ */
 class Orders extends Revenue{
     private $_transactionID;
     private $_category;
     private $_item;
-    private $_amount; // String
+    private $_amount;
 
+    /**
+     * Constructor instantiates an Orders object
+     * @param date $date date of order
+     * @param int $totalAmount total amount for order
+     * @param int $transactionID identification number for transaction
+     * @param string $category category of food
+     * @param string $item food item
+     * @param int $amount quantity of food
+     */
     public function __construct($date, $totalAmount,
                                 $transactionID, $category, $item, $amount)
     {
@@ -19,11 +34,22 @@ class Orders extends Revenue{
     //*****************
     //     SETTERS
     //*****************
+
+    /**
+     * Set the item
+     * @param string $item item
+     * @return void
+     */
     public function setItem($item)
     {
         $this->_item = $item;
     }
 
+    /**
+     * Set the quantity
+     * @param float $amount quantity
+     * @return void
+     */
     public function setAmount($amount)
     {
         $this->_amount = $amount;
@@ -32,22 +58,39 @@ class Orders extends Revenue{
     //*****************
     //     GETTERS
     //*****************
+
+    /**
+     * Get the items
+     * @return string food item
+     */
     public function getItems()
     {
         return $this->_item;
     }
+
+    /**
+     * Get the quantity
+     * @return int quantity
+     */
     public function getAmount()
     {
         return $this->_amount;
     }
+
+    /**
+     * Get the transactionID
+     * @return int identification number for transaction
+     */
     public function getTransactionID()
     {
         return $this->_transactionID;
     }
+
+    /**
+     * Get the category
+     * @return string category of food
+     */
     public function getCategory(){
         return $this->_category;
-    }
-    public function getQuantity(){
-        return $this->_amount;
     }
 }
